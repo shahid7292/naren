74e7e15bf5d30573db2c83f25868789d332bac7d
shahidraza <shahidraza.moulvi@aimedatsolutions.com>
2023-08-20 14:55:06 +0530

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import Providers from "./providers";
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
